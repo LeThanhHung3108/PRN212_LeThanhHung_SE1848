07866165969afe7db221510f5899c475f68d83e2
Lê Thanh Hùng <lethanhhung1082004@gmail.com>
2025-06-17 13:32:42 +0700

    CheckBox_Event

--- ButtonControl_TongHop/obj/Debug/net9.0-windows/ButtonControl_TongHop.AssemblyInfo.cs
+++ ButtonControl_TongHop/obj/Debug/net9.0-windows/ButtonControl_TongHop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ButtonControl_TongHop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16dec8b1e6abfcbfc59844ef087ed22fabc074b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+504da1d6305bc7343abc6428d8590fa57b0d9b73")]
 [assembly: System.Reflection.AssemblyProductAttribute("ButtonControl_TongHop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ButtonControl_TongHop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ HelloWpfApp/LoginWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelloWpfApp
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExist_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            //Purpose account is:
+            //username: hungthanh
+            //password: 10082004
+            if(txtUsername.Text == "hungthanh" && txtPassword.Password == "10082004")
+            {
+                // MainWindow mainWindow = new MainWindow();
+                //mainWindow.Show();
+                //Close the login window
+                //Close();
+                //or call
+                btnExist.RaiseEvent(e);
+            }
+            else
+            {
+                MessageBox.Show("Login failled !!");
+            }
+        }
+    }
+}
